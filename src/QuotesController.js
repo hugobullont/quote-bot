f2ffe848ccf9b1fbaf3eeda8033505ee8b2b9ab5
Hugo Bullon <hugobullont@gmail.com>
2020-08-03 20:32:15 -0500

    fix

--- src/QuotesController.js
+++ src/QuotesController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 
 let apiURL = 'http://localhost:' + process.env.PORT;
 
