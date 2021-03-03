e7ab82ef0beeb47dd959d7fb520b89055a91150c
Hugo Bullon <hugobullont@gmail.com>
2021-03-03 10:34:42 -0500

    changes to sech

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ client.on('message', async msg => {
         }
 
         if(msg.content === '!sech') {
-            GifController.getRandomGIFbyTag('sech-music').then((value) => {
+            GifController.getRandomGIFbyTag('sech reggaeton').then((value) => {
                 msg.channel.send(value.embed_url);
             });
         }
