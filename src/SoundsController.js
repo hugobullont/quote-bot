df055551e854b1a1f3f38db9204a9a2925b6b850
Hugo Bullon <hugobullont@gmail.com>
2021-03-22 23:10:17 -0500

    fix

--- src/SoundsController.js
+++ src/SoundsController.js
@@ -174,7 +174,7 @@ exports.playJeepeta = async (connection, msg) => {
 exports.playChupasela = async (connection, msg) => {
     const dispatcher = connection.play('./assets/chupasela.mp3');
 
-    dispatcher.setVolume(0.7);
+    dispatcher.setVolume(0.9);
 
     dispatcher.on('finish', () => {
         msg.member.voice.channel.leave();
